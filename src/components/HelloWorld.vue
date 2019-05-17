61479b1ae1d4cb01fb18cc4d73368945e824d1ab
Saksham <gupta.10@iitj.ac.in>
2019-05-17 10:55:59 +0530

    Added Client And Server

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,164 @@
+
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-layout text-xs-center wrap>
+      <v-flex xs12>
+        <v-img :src="require('../assets/zolologo.jpg')" class="my-3" contain height="200"></v-img>
+      </v-flex>
+
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">Welcome to Food Menu Creation</h1>
+        <p class="subheading font-weight-regular">
+          Using this you can create your own menu,
+          <br>
+        </p>
+      </v-flex>
+      <v-form>
+        <v-container>
+          <v-layout>
+            <v-flex xs12 md4>
+              <v-text-field label="User Name" required></v-text-field>
+            </v-flex>
+
+            <v-flex xs12 md4>
+              <v-select :items="items1" label="User Hotel" required></v-select>
+            </v-flex>
+
+            <v-flex xs12 md4>
+              <v-select :items="items" label="User City" required></v-select>
+            </v-flex>
+            
+            
+          </v-layout>
+        </v-container>
+      </v-form>
+      <v-flex xs12 v-for="index in tables" :key="index">
+
+        <table >
+
+          <template align="right">
+            <tr>
+            <th>{{ getDate(date_picker, index) }}</th>
+            <th v-for="index in tables1" :key="index">Item {{index}}</th>
+          </tr>
+          <tr>
+            <th>Breakfast</th>
+            <td v-for="index in tables1" :key="index">
+              <input :name="index" type="text" class="input" v-model="table.breakfast.item[index-1]">
+            </td>
+          </tr>
+
+          <tr>
+            <th>Lunch</th>
+            <td v-for="index in tables1" :key="index">
+              <input :name="index" type="text" class="input" v-model="table.lunch.item[index-1]">
+            </td>
+          </tr>
+
+          <tr>
+            <th>Dinner</th>
+            <td v-for="index in tables1" :key="index">
+              <input :name="index" type="text" class="input" v-model="table.dinner.item[index-1]">
+            </td>
+          </tr>
+          </template>
+        </table>
+        <br>
+        <br>
+        <br>
+      </v-flex>
+      <br>
+      <v-flex xs12>
+        <v-date-picker
+          v-model="date_picker"
+          ref="picker"
+          min=""
+        ></v-date-picker>
+      </v-flex>
+    </v-layout>
+
+    <v-btn fab @click="tables++">
+      Add Day <v-icon>add</v-icon>
+    </v-btn>
+    <br>
+
+    <v-btn fab @click="tables1++">
+      Add Items<v-icon>add</v-icon>
+    </v-btn>
+
+
+
+    <span>{{ date_picker }}</span>
+    <br>
+    <span>{{ table }}</span>
+  </v-container>
 </template>
 
 <script>
+import axios from "axios" 
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      headers: [
+        {
+          text: "Add Calendar Here",
+          align: "left",
+          sortable: false,
+          value: "name"
+        },
+        { text: "Item1", value: "calories" },
+        { text: "Item2", value: "fat" },
+        { text: "Item3", value: "carbs" },
+        { text: "Item4", value: "protein" },
+        { text: "Item5", value: "iron" }
+      ],
+      meals: [
+        {
+          name: "Breakfast"
+        },
+        {
+          name: "Lunch"
+        },
+        {
+          name: "Dinner"
+        }
+      ],
+      tables: 1,
+      tables1: 2,
+      table: {
+        breakfast: {
+          item: []
+        },
+        lunch: {
+          item: []
+        },
+        dinner: {
+          item: []
+        }
+      },
+
+      items: ['Pune','Bengaluru','Hyderabad'],
+      items1: ['Zolo1','Zolo2','Zolo3','Zolo4'],
+    };
+  },
+  methods: {
+    getDate(date, index){
+      return date + ' ' + (index - 1)
+    }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+input {
+  width: 100px;
+  border: 1px solid #acacac;
+  margin: 0;
+  padding: 2px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+textarea {
+  width: 100px;
+  height: 100px;
+  border: 1px solid #acacac;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
