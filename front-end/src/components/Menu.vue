bc705e54c960eadff8d13e2f6cc86129469db56a
Saksham <gupta.10@iitj.ac.in>
2019-05-19 23:34:06 +0530

    Updated menu.vue

--- front-end/src/components/Menu.vue
+++ front-end/src/components/Menu.vue
@@ -9,7 +9,7 @@
                 type="text"
                 value="username"
                 placeholder="User Name"
-                v-model="userName"
+                v-model="newData.userName"
               >
             </th>
             <th>
@@ -17,7 +17,7 @@
                 type="text"
                 value="usercity"
                 placeholder="User City"
-                v-model="userCity"
+                v-model="newData.userCity"
               >
             </th>
             <th>
@@ -25,7 +25,7 @@
                 type="text"
                 value="userhotel"
                 placeholder="User Hotel"
-                v-model="userHotel"
+                v-model="newData.userHotel"
               >
             </th>
           </tr>
@@ -35,7 +35,7 @@
         <table>
           <tr>
             <th>
-              <input type="date" v-model="selectedDate">
+              <input type="date" v-model="newData.selectedDate">
             </th>
             <th>Item 1</th>
             <th>Item 2</th>
@@ -78,17 +78,18 @@
             </td>
           </tr>
         </table>
-        <v-btn @click="addNewTableElementBreakfast">Add Data</v-btn>
+        <v-btn @click="addNewTableElementBreakfast()">Add Data</v-btn>
         <br>
         <br>
         <br>
       </v-flex>
 
       <v-flex sm12>
-        <table v-for="(menu, date, i) in data" :key="i">
+        <table v-for="(menu, date, i) in data1" :key="i">
+          <!-- menu:{{menu}} -->
           <tr>
             <th>
-              <input type="text" :value="date" readonly>
+              <input type="text" :value="menu.date" readonly>
             </th>
             <th>Item 1</th>
             <th>Item 2</th>
@@ -142,76 +143,31 @@ import axios from "axios";
 export default {
   data() {
     return {
-      data: {
-        // "10-05-2019": {
-        //   breakfast: [
-        //     "item1",
-        //     "item2",
-        //     "item3",
-        //     "item4",
-        //     "item5",
-        //     "item6",
-        //     "item7"
-        //   ],
-        //   lunch: [
-        //     "item1",
-        //     "item2",
-        //     "item3",
-        //     "item4",
-        //     "item5",
-        //     "item6",
-        //     "item7"
-        //   ],
-        //   dinner: [
-        //     "item1",
-        //     "item2",
-        //     "item3",
-        //     "item4",
-        //     "item5",
-        //     "item6",
-        //     "item7"
-        //   ]
-        // },
-        // "11-05-2019": {
-        //   breakfast: [
-        //     "item1",
-        //     "item2",
-        //     "item3",
-        //     "item4",
-        //     "item5",
-        //     "item6",
-        //     "item7"
-        //   ],
-        //   lunch: [
-        //     "item1",
-        //     "item2",
-        //     "item3",
-        //     "item4",
-        //     "item5",
-        //     "item6",
-        //     "item7"
-        //   ],
-        //   dinner: [
-        //     "item1",
-        //     "item2",
-        //     "item3",
-        //     "item4",
-        //     "item5",
-        //     "item6",
-        //     "item7"
-        //   ]
-        // }
-      },
-      tableName: "meals",
-      userCity: "",
-      userName: '',
-      userHotel: '',
-      selectedDate: "",
+      data1: {
+        
+        date:{breakfast: ['bs','s'],
+        lunch: [],
+        dinner: [],
+        userCity: '',
+        userName: '',
+        userHotel: '',
+        date: ''}
+        
+        },
+      // tableName: "meals",
+      // userCity: "",
+      // userName: '',
+      // userHotel: '',
+      // selectedDate: "",
       // finalEntry : "",
       newData: {
         breakfast: [],
         lunch: [],
-        dinner: []
+        dinner: [],
+        userCity: '',
+        userName: '',
+        userHotel: '',
+        selectedDate: ''
       },
       
     };
@@ -221,21 +177,52 @@ export default {
     //   this.user
     // },
     addNewData() {
-      this.data["" + this.selectedDate] = this.newData;
+      this.data["" + this.newData.selectedDate] = this.newData;
       // this.newData = { breakfast: [], lunch: [], dinner: [],  };
     },
     saveAllData() {
       localStorage.setItem("data-123", JSON.stringify(this.data));
     },
-    addNewTableElementBreakfast() {
+    async addNewTableElementBreakfast() {
       console.log(this.newData);
-      const finalEntry = this.userCity + this.userName + this.userHotel + this.selectedDate,
-      this.newData['PriKey'] = finalEntry,
-      axios.post(
+      // const finalEntry = this.userCity + this.userName + this.userHotel + this.selectedDate,
+      // this.newData['username'] = this.user,
+      await axios.post(
         `http://localhost:3000/meals/insert`,
         
         this.newData
       );
+
+      await this.getAllData();
+    },
+    async getAllData() {
+      // const tableSchema = await axios.get(
+      //   `http://localhost:3000/breakfast/schema`
+      // );
+      const tableData = await axios.get(
+        `http://localhost:3000/meals`
+      );
+      // this.tableSchema = tableSchema.data;
+      this.data1 = tableData.data;
+
+      await this.printTable();
+      
+    },
+    async printTable() {
+      let index = 0;
+      for(index =0;index<this.data1.length;index++){
+       
+        this.data1[index].breakfast = this.data1[index].breakfast.split(',') 
+        this.data1[index].lunch = this.data1[index].lunch.split(',') 
+        this.data1[index].dinner = this.data1[index].dinner.split(',') 
+      }
+      // for(index )
+      // console.log('breakfast', this.data1[index].breakfast, typeof(this.data1[0]));
+      // this.data1.breakfast=tableData.data.breakfast.split(',');
+
+
+      console.log(this.data1);
+
     }
   },
   created() {
